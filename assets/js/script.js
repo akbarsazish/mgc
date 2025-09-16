a7461d9e0f003b5e9c5c5d32be18d917979ebe97
akbarsazish <sazishaliakbar@gmail.com>
2025-09-16 09:05:00 +0330

    changed some parts

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,42 @@
  });
 
 
+
+ 
+//  connected programs
+  var swiper = new Swiper("#connectedPrograms", {
+      spaceBetween: 10,
+      slidesPerView: 2,
+      grabCursor: true,
+      loop:true,
+      zoom: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      breakpoints: {
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 5,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 7,
+          spaceBetween: 20,
+        },
+      },
+  });
+
+
 //  most popular destination
   var swiper = new Swiper("#popularDestination", {
       effect: "coverflow",
       grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 4,
+      slidesPerView: 3,
       loop:true,
       coverflowEffect: {
         rotate: 50,
@@ -26,6 +56,21 @@
       pagination: {
         el: ".swiper-pagination",
       },
+
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+        },
+      },
     });
 
 
@@ -33,10 +78,27 @@
   var swiper = new Swiper("#bestHotels", {
       centeredSlides: true,
       slidesPerView: 4,
-      spaceBetween: 30,
+      spaceBetween: 10,
       grabCursor: true,
       loop:true,
       pagination: {
         el: ".swiper-pagination",
       },
+
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 30,
+        },
+      },
     });
+
+
